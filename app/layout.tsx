7f4a98c2317b5d0a8ec411639ce625db81445866
Tegar Ibrahim <57317804+xkyrage@users.noreply.github.com>
2026-05-20 11:42:28 +0700

    Remove metadata export from layout
    Removed metadata export for title and description.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,7 @@
-export const metadata = {
-  title: "Mini Kibana Log Debugger",
-  description: "Parse logs like Kibana"
-};
-
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
